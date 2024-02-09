6d0e76fa71c58cc5cd0844682a2980f6d4865704
mathangisd <mathangi.subramanian@gmail.com>
2024-02-09 09:54:56 -0500

    Changes to header, text truncation and avatar image position.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,17 +29,58 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+        </DesktopActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubLink href='/'>Already a Subscriber?</SubLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
 };
 
+
+
+
+const SubscribeWrapper = styled.div`
+  display: none;
+  @media ${QUERIES.laptopAndUp} { 
+      display: revert;
+      position: relative;
+      justify-self: end;
+    }
+
+`;
+
+const SubLink = styled.a`
+  font-size: 0.875rem;
+  color: var(--color-gray-900);
+  font-style: italic;
+  text-decoration: underline;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  margin-top: 8px;
+`;
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
+
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
@@ -59,12 +100,35 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+  @media ${QUERIES.laptopAndUp} { 
+    display: flex;
+  }
+`;
+
+
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    align-items: center;
+    justify-content: revert;
+    justify-items: start;
+    grid-template-columns: 1fr auto 1fr;
+    margin-top: 16px;
+    margin-bottom: 72px;
+   }
 `;
 
 export default Header;
